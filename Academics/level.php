cec1c105c5190b45914d64e6c73e93034982c13e
Abdulkadir <ahi.nuune@mgail.com>
2021-05-31 17:53:56 +0300

    Level update processing

--- Academics/level.php
+++ Academics/level.php
@@ -1,4 +1,5 @@
 <?php include('../path.php'); ?>
+<?php include(ROOT_PATH . 'includes/functions.php'); ?>
 <?php $title = "Level"; include_once(ROOT_PATH. 'includes/layouts/header.php'); ?>
       <!-- .app-header -->
         <?php include_once(ROOT_PATH. 'includes/layouts/nav.php'); ?>
@@ -18,6 +19,42 @@
               <div class="page-section">
                 <div class="row">
                     <div class="col-md-4">
+                    <?php
+                      if(isset($_GET['id'])):
+                    ?>
+                    <form id="edit_level_form">
+                        <div class="card">
+                            <div class="card-header bg-info text-light"> Edit Level</div>
+                            <div class="card-body">
+                              <span id="msg"></span>
+                                <div class="row">
+                                <?php $id = $_GET['id']; 
+                                  $sql = "SELECT * FROM levels WHERE id = " . $id;
+                                  $resultQuery = mysqli_query($con, $sql);
+                                  $count = mysqli_num_rows($resultQuery);
+                                  if( $count > 0 ):
+                                      $levelRecord = mysqli_fetch_assoc($resultQuery);
+                                ?>
+                                  <div class="form-group col-md-12">
+                                        <input type="text" name="level_id" id="level_id" class="form-control" placeholder='1' value="<?php echo $levelRecord['id']; ?>">
+                                    </div>
+                                    <div class="form-group col-md-12">
+                                        <label for="">level name</label>
+                                        <input type="text" name="level_name" id="level_name" class="form-control" placeholder='Class One' value="<?php echo $levelRecord['name']; ?>">
+                                    </div>
+                                    <div class="form-group col-md-12">
+                                        <label for="">Description</label>
+                                        <input type="text" name="description" id="description" class="form-control" placeholder='....' value="<?php echo $levelRecord['description']; ?>">
+                                    </div>                                   
+                                    <div class="form-group col-md-12">
+                                        <button type="submit" name="edit_level" class="btn btn-info float-right" id='edit_level'> Update</button>
+                                    </div>
+                                <?php else: header('Location: ../index.php'); endif; ?>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                  <?php else: ?>
                     <form id="save_level_form">
                         <div class="card">
                             <div class="card-header bg-info text-light"> Add new level </div>
@@ -39,6 +76,7 @@
                             </div>
                         </div>
                     </form>
+                  <?php endif; ?>
                     </div>
                     <div class="col-md-8">
                     <div class="card">
@@ -54,17 +92,22 @@
                                 </tr>
                               </thead>
                               <tbody id='load_level_table'>
+                              <?php
+                                $sql = "SELECT * FROM levels";
+                                $record = getAllRecord($sql); 
+                              foreach($record as $key => $recordLevel): ?>
                                   <tr>
-                                    <td>1</td>
-                                    <td>Hoose</td>
-                                    <td>Dugsi Hoose</td>
+                                    <td><?php echo $key + 1; ?></td>
+                                    <td><?php echo $recordLevel['name']; ?></td>
+                                    <td><?php echo $recordLevel['description']; ?></td>
                                     <td>
-                                      <div class='btn-group btn-group-sm' role='group' aria-label='Basic example'>
-                                        <button type='button' class='btn btn-info' data-toggle='modal' data-target='#VeiwOrder'  data-toggle='tooltip' data-placement='top' title='View student information' id='btnviewOrder' data-id='$id'><i class='fa fa-list'></i></button>
+                                      <div class='btn-group btn-group-sm' role='group'>
+                                        <a href="level.php?id=<?php echo $recordLevel['id']; ?>" class='btn btn-secondary'><i class='fa fa-edit'></i></a>
                                         <button type='button' class='btn btn-danger' id='deleteOrder' data-id='$id'  data-toggle='tooltip' data-placement='top' title='Delete'> <i class='fa fa-trash'></i> </button>
                                       </div>
                                     </td>
                                   </tr>
+                                <?php endforeach; ?>
                               </tbody>
                 
                           </table>
@@ -120,7 +163,41 @@
             // alert(level_name);
             // alert(description);
           }
+        });
 
-        })
+        $('#edit_level_form').submit(function(e){
+          event.preventDefault();
+          var level_name = $('#level_name').val();
+          var description = $('#description').val();
+
+          if( level_name == "" || description == "" ){
+            $('#msg').fadeIn().html('<div class="alert alert-warning"> <a href="" class="close" data-dismiss="alert" aria-label="close">&times;</a> All fields are required. please fill it. </div>');
+            // alert('All fields are required. please fill it.');
+            setTimeout(() => {
+              $('#msg').fadeOut();
+            }, 4000);
+          }else{
+
+            $.ajax({
+              url: DOMAIN + "/includes/insert.php?action=update",
+              method: "POST",
+              data: $(this).serialize(),
+              success: function(data){
+                $('#msg').fadeIn().html(data);
+                // console.log(data);
+                setTimeout(() => {
+                  $('#msg').fadeOut();
+                }, 4000);
+              }
+            });
+
+            // alert(level_name);
+            // alert(description);
+          }
+        });
+      });
+
+      $('#btnEditLevel').click(function(){
+        alert('Make Edit.');
       })
     </script>
--- includes/insert.php
+++ includes/insert.php
@@ -20,6 +20,20 @@ if( $action == "insert" ) {
         echo '<div class="alert alert-danger"> <a href="" class="close" data-dismiss="alert" aria-label="close">&times;</a> Query failed. </div>';
     }
     // echo $level_name . " " . $description;
+}else if( $action == "update" ) {
+    extract($_POST);
+
+    $level_name = mysqli_real_escape_string($con, $level_name);
+    $description = mysqli_real_escape_string($con, $description);
+
+    $sql = "UPDATE levels SET name = '$level_name', description = '$description' WHERE id = 
+    $resultSet = mysqli_query($con, $sql);
+    if( $resultSet ){
+        echo '<div class="alert alert-success"> <a href="" class="close" data-dismiss="alert" aria-label="close">&times;</a> Operation Done Successfully. </div>';
+    }else{
+        echo '<div class="alert alert-danger"> <a href="" class="close" data-dismiss="alert" aria-label="close">&times;</a> Query failed. </div>';
+    }
+    // echo $level_name . " " . $description;
 }
 
 // echo $action;
